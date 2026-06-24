e9ae560a884bb4480c5ca27a2d1fcaed088058a1
dvianisoho <dviani@sohosquared.com>
2026-06-24 08:19:51 -0400

    fix: AudioSegment conversion for NumPy 2

--- nemo/collections/asr/parts/preprocessing/segment.py
+++ nemo/collections/asr/parts/preprocessing/segment.py
@@ -259,14 +259,15 @@ class AudioSegment(object):
         Audio sample type is usually integer or float-point.
         Integers will be scaled to [-1, 1] in float32.
         """
-        float32_samples = samples.astype('float32')
-        if samples.dtype in np.sctypes['int']:
+        float32_samples = samples.astype("float32")
+        if samples.dtype in (np.int8, np.int16, np.int32, np.int64):
             bits = np.iinfo(samples.dtype).bits
             float32_samples *= 1.0 / 2 ** (bits - 1)
-        elif samples.dtype in np.sctypes['float']:
+        elif samples.dtype in (np.float16, np.float32, np.float64):
             pass
         else:
             raise TypeError("Unsupported sample type: %s." % samples.dtype)
+
         return float32_samples
 
     @classmethod
